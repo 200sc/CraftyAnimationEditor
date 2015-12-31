000028372e3ca256cba67113e207384feb209d32
Patrick Stephen <patrick.stephen@nerdery.com>
2015-12-31 13:44:24 -0600

    Graphics tweaks, renaming color settings

--- settings.js
+++ settings.js
@@ -15,5 +15,24 @@ Settings = {
     label_prefix: "spr_",
     file_directory_prefix: "assets/",
     // components required by generated components, comma-seperated
-    def_components: "SpriteAnimation"
+    def_components: "SpriteAnimation",
+    baseColor: {
+        r: 20,
+        g: 100,
+        b: 140
+    }
+}
+
+Colors = {
+    bg: 'rgb('+Settings.baseColor["r"]+','+Settings.baseColor["g"]+','+Settings.baseColor["b"]+')',
+    side: 'rgb('+(Settings.baseColor["r"]+50)+','+(Settings.baseColor["g"]+50)+','+(Settings.baseColor["b"]+50)+')',
+    side2: 'rgb('+(Settings.baseColor["r"]+20)+','+(Settings.baseColor["g"]+10)+','+(Settings.baseColor["b"]+20)+')',
+    itm1: 'rgb('+(Settings.baseColor["r"]+90)+','+(Settings.baseColor["g"]+50)+','+(Settings.baseColor["b"]+90)+')',
+    itm2: 'rgb('+(Settings.baseColor["r"]+30)+','+(Settings.baseColor["g"]+50)+','+(Settings.baseColor["b"]+70)+')',
+    anim: 'rgba('+(Settings.baseColor["r"]+80)+','+(Settings.baseColor["g"])+','+(Settings.baseColor["b"]+80)+',.75)',
+    tab: 'rgba('+(Settings.baseColor["r"]+20)+','+(Settings.baseColor["g"]+10)+','+(Settings.baseColor["b"]+20)+',.75)',
+    bot: 'rgba('+(Settings.baseColor["r"]-10)+','+(Settings.baseColor["g"]-30)+','+(Settings.baseColor["b"]-10)+',.75)',
+    shd: 'rgb(200,200,255)',
+    blk: "rgb(0,0,0)",
+    wht: "rgb(255,255,255)"
 }
